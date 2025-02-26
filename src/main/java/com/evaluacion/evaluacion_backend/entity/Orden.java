f904280d0a1c3cf7586d0441eccd2fad2b5652e6
Jose Villacorta <villacortafur@gmail.com>
2025-02-26 01:34:36 -0500

    Agregar entidad

--- src/main/java/com/evaluacion/evaluacion_backend/entity/Orden.java
+++ src/main/java/com/evaluacion/evaluacion_backend/entity/Orden.java
@@ -1,4 +1,48 @@
 package com.evaluacion.evaluacion_backend.entity;
 
-public class Orden {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Orden")
+public class Orden implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
--- src/main/java/com/evaluacion/evaluacion_backend/entity/Usuario.java
+++ src/main/java/com/evaluacion/evaluacion_backend/entity/Usuario.java
@@ -1,4 +1,50 @@
 package com.evaluacion.evaluacion_backend.entity;
 
-public class Usuario {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "Usuario")
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank
+    @Column(name = "nombre")
+    private String nombre;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Orden> ordens;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Orden> getOrdens() {
+        return ordens;
+    }
+
+    public void setOrdens(List<Orden> ordens) {
+        this.ordens = ordens;
+    }
 }
